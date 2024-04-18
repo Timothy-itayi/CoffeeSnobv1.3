49f8d9839a356141b5f082c99c9da21c28819e82
Timothy-itayi <119027453+Timothy-itayi@users.noreply.github.com>
2024-04-18 14:35:11 +1200

    Update RouteViewController.swift

--- CoffeeSnob/Controller /New Zealand/RouteViewController.swift
+++ CoffeeSnob/Controller /New Zealand/RouteViewController.swift
@@ -108,7 +108,7 @@ class RouteViewController: UIViewController, MapViewDelegate, CLLocationManagerD
         let styleContainer = StyleContainer.defaultStyle
         let cameraUpdate = CameraUpdate(position: CLLocationCoordinate2D(latitude: -37.77, longitude: 175.28), zoom: 11, tilt: 45, rotation: 0)
         let resourceCachePolicy = OnDiskCachePolicy.cache(duration: Measurement.tt.hours(10), maxSize: Measurement.tt.megabytes(200))
-        let mapOptions = MapOptions(mapStyle: styleContainer, apiKey: "pI4kmVlG64hsmsxz9fLKoMtXOmLVgaJW", cameraUpdate: cameraUpdate, cachePolicy: resourceCachePolicy)
+        let mapOptions = MapOptions(mapStyle: styleContainer, apiKey: "", cameraUpdate: cameraUpdate, cachePolicy: resourceCachePolicy)
         
         DispatchQueue.main.async { [weak self] in
             guard let self = self else { return }
